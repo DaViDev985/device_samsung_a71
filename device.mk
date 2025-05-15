6f55380941280d958b215fdb3203ec6fab19170b
DaViDev985 <davidev985@gmail.com>
2025-05-15 17:39:31 +0200

    device.mk: Add DerpFest flags

--- device.mk
+++ device.mk
@@ -9,6 +9,11 @@ DEVICE_PATH := device/samsung/a71
 # Inherit Common Device Tree
 $(call inherit-product, device/samsung/a71-common/common.mk)
 
+# DerpFest Flags
+DERPFEST_BUILD_TYPE := Official
+DERP_VERSION_APPEND_TIME_OF_DAY=true
+TARGET_NOT_USES_BLUR=true
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/vendor/etc/mixer_paths_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp.xml \
